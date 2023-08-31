8489263b4f5594e4b5ee3a0252ebcb9a26979215
Ricardo Alves <dev.rical21@gmail.com>
2023-08-31 11:08:00 -0300

    Criando método GET e incluindo Paginação e Ordenação de registros devolvidos pela Controller

--- clinica/src/main/java/api/rest/clinica/controller/MedicoController.java
+++ clinica/src/main/java/api/rest/clinica/controller/MedicoController.java
@@ -1,15 +1,18 @@
 package api.rest.clinica.controller;
 
 import api.rest.clinica.medico.DadosCadastroMedico;
+import api.rest.clinica.medico.DadosListagemMedico;
 import api.rest.clinica.medico.Medico;
 import api.rest.clinica.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,5 +26,13 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
+    }
+
+
 
 }

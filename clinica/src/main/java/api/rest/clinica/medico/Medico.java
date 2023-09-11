be536ef5c8b342e4d54b0e19f4f608ad0b8626b8
Ricardo Alves <dev.rical21@gmail.com>
2023-09-10 21:55:38 -0300

    Padronizando as respostas da API com ResponseEntity

--- clinica/src/main/java/api/rest/clinica/medico/Medico.java
+++ clinica/src/main/java/api/rest/clinica/medico/Medico.java
@@ -30,6 +30,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.crm();
@@ -37,5 +39,25 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null) {
+            this.endereco.atualizarEndereco(dados.endereco());
+        }
+
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
